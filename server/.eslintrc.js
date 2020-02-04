9ed46a24b75f409ed72f97bda23278b464b516bf
Vlad <v.y.beletskiy@gmail.com>
2020-02-05 00:44:11 +0200

    update server eslint config

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -9,9 +9,15 @@ module.exports =  {
    parserOptions:  {
       ecmaVersion:  2019,  // Allows for the parsing of modern ECMAScript features
       sourceType:  'module',  // Allows for the use of imports
+      project: "./server/tsconfig.json",
     },
     rules:  {
+      '@typescript-eslint/no-var-requires': 0,
       // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
       // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     },
+    env: {
+      node: true,
+      es6: true,
+    }
   };
